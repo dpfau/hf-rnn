3e408521cdf19eae7cd8e8e65f63dca8e8fcaeec
David Pfau <hotmail.com@gmail.com>
2011-09-14 23:11:05 -0400

    Added testing script for hessian-vector multiplication.  Tested hess_v for pointwise nonlinearities.

--- hess_v.m
+++ hess_v.m
@@ -1,4 +1,4 @@
-function Rparams = hess_v( x, y, params, v_params, e, g, Je, Jg, He, Hg, dL, ddL )
+function [Rparams Ry Rh Rt Rs Rdh Rdt Rds] = hess_v( x, y, params, v_params, e, g, Je, Jg, He, Hg, dL, ddL )
 % Calculates the product of the Hessian of the error wrt the weights with
 % an arbitrary vector v according to the method of Pearlmutter 1994.
 %
@@ -28,36 +28,38 @@ Rht = vh0;
 ht  = h0;
 for i = 1:size(x,2)
     Rt(:,i) = W_hh * Rht + vW_hh * ht + vW_hx * x(:,i) + vb_h;
-    Rh(:,i) = Je( Rt(:,i) ) * Rt(:,i);
+    Rh(:,i) = Je( t(:,i) ) * Rt(:,i);
     Rht = Rh(:,i);
     ht  =  h(:,i);
     
     Rs(:,i) = W_yh * Rht + vW_yh * ht + vb_y;
-    Ry(:,i) = Jg( Rs(:,i) ) * Rs(:,i);
+    Ry(:,i) = Jg( s(:,i) ) * Rs(:,i);
 end
 
 % Backward Pass
 [~, dt, ds] = bptt( x, y, params, e, g, Je, Jg, dL, y_est, h, t, s );
 
-Rdt = zeros(size(h0)); % R_v[dL/dt], where L is the loss
+Rdt = zeros(size(dt)); % R_v[dL/dt], where L is the loss
+Rds = zeros(size(ds));
+Rdh = zeros(size(dt));
 dtt = zeros(size(h0)); % dt at the current time step
 for i = size(x,2):-1:1
-    Rds = Jg( s(:,i) )' * ( ddL( y(:,i), y_est(:,i) ) .* Ry(:,i) ) + ...
+    Rds(:,i) = Jg( s(:,i) )' * ( ddL( y(:,i), y_est(:,i) ) .* Ry(:,i) ) + ...
        tprod( Hg( s(:,i) ), [2 1 -1], Rs(:,i), -1 ) * dL( y(:,i), y_est(:,i) ); % R_v[dL/ds]
-    RW_yh = RW_yh + Rds * h(:,i)' + ds(:,i) * Rh(:,i);
-    Rb_y  = Rb_y  + Rds;
+    RW_yh = RW_yh + Rds(:,i) * h(:,i)' + ds(:,i) * Rh(:,i)';
+    Rb_y  = Rb_y  + Rds(:,i);
     
-    Rdh = ( W_hh' * Rdt + vW_hh' * dtt ) + ( W_yh' * Rds + vW_yh' * ds(:,i) );    
-    Rdt = Je( t(:,i) )' * Rdh + tprod( He( t(:,i) ), [2 1 -1], Rt(:,i), -1 ) * ( W_yh' * ds(:,i) + W_hh' * dtt );
+    Rdh(:,i) = ( W_hh' * Rdt(:,i) + vW_hh' * dtt ) + ( W_yh' * Rds(:,i) + vW_yh' * ds(:,i) );    
+    Rdt(:,i) = Je( t(:,i) )' * Rdh + tprod( He( t(:,i) ), [2 1 -1], Rt(:,i), -1 ) * ( W_yh' * ds(:,i) + W_hh' * dtt );
     if i > 1
-        RW_hh = RW_hh + Rdt * h(:,i-1)' + dt(:,i) * Rh(:,i-1)';
+        RW_hh = RW_hh + Rdt(:,i) * h(:,i-1)' + dt(:,i) * Rh(:,i-1)';
     else
-        RW_hh = RW_hh + Rdt * h0' + dt(:,i) * vh0;
+        RW_hh = RW_hh + Rdt(:,i) * h0' + dt(:,i) * vh0';
     end
-    RW_hx = RW_hx + Rdt * x(:,i)';
-    Rb_h  = Rb_h + Rdt;
+    RW_hx = RW_hx + Rdt(:,i) * x(:,i)';
+    Rb_h  = Rb_h + Rdt(:,i);
     dtt = dt(:,i);
 end
 
-Rh0 = vW_hh' * dt + W_hh' * Rdt;
+Rh0 = vW_hh' * dtt + W_hh' * Rdt(:,1);
 Rparams = { Rh0, RW_hh, RW_hx, RW_yh, Rb_h, Rb_y };
---
+++ test_hess_v.m
@@ -0,0 +1,66 @@
+% Numerically verify the correctness of my implementation of
+% Hessian-vector multiplication.  David Pfau, 2011
+
+n = 5;
+m = 3;
+k = 3;
+t = 1;
+
+W_hh = 0.1 * randn(n);
+W_hx = randn(n,m);
+W_yh = 0.1 * randn(k,n);
+
+b_h = 0.1 * randn(n,1);
+b_y = 0.1 * randn(k,1);
+
+x = randn(m,t);
+y = rand(k,t);
+y = y./(ones(k,1)*sum(y));
+h_0 = randn(n,1);
+
+params = { h_0, W_hh, W_hx, W_yh, b_h, b_y };
+
+g = @tanh;
+Jg = @(x) diag(1 - tanh(x).^2);
+Hg = @(x) diag_tensor(-2*tanh(x).*(1-tanh(x).^2),3);
+e = @(x) exp(x)/sum(exp(x)); % softmax
+Je = @(x) diag(exp(x)/sum(exp(x))) - exp(x)*exp(x)'/sum(exp(x))^2;
+He = @(x) 2*tprod( exp(x)*exp(x)', [1 2], exp(x), 3 )/sum(exp(x))^3 ...
+    ;
+
+% g = @(x) x;
+% Jg = @(x) eye(length(x));
+% Hg = @(x) zeros(length(x),length(x),length(x));
+% e = @(x) x;
+% Je = @(x) eye(length(x));
+% He = @(x) zeros(length(x),length(x),length(x));
+
+diff_params = cell(6,1); % the Hessian-vector product computed by difference of gradients
+v_params = cell(6,1); % the vector with which we multiply the Hessian
+for i = 1:length(diff_params)
+    diff_params{i} = zeros(size(params{i}));
+    v_params{i} = randn(size(params{i}));
+end
+
+eps = 1e-9;
+
+[y0 h0 t0 s0] = rnn( x, params, g, e );
+[dparams0 dt0 ds0] = bptt( x, y, params, g, e, Jg, Je, @dXH, y0, h0, t0, s0 );
+for i = 1:length(params)
+    params{i} = params{i} + eps * v_params{i};
+end
+
+[y1 h1 t1 s1] = rnn( x, params, g, e );
+[dparams1 dt1 ds1] = bptt( x, y, params, g, e, Jg, Je, @dXH, y1, h1, t1, s1 );
+for i = 1:length(params)
+    diff_params{i} = ( dparams1{i} - dparams0{i} ) / eps;
+    params{i} = params{i} - eps * v_params{i};
+end
+
+vars = { 'y', 'h', 't', 's', 'dt', 'ds' };
+for i = 1:length(vars)
+    var = vars{i};
+    eval(['diff_' var ' = ( ' var '1 - ' var '0 ) / eps;'])
+end
+
+[Rparams Ry Rh Rt Rs Rdh Rdt Rds] = hess_v( x, y, params, v_params, g, e, Jg, Je, Hg, He, @dXH, @ddXH );
