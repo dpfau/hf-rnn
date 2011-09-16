276e36bc73f71f3225c4ef0d343f2650c103404f
David Pfau <hotmail.com@gmail.com>
2011-09-16 19:15:35 -0400

    Removed unneccesary paths from init

--- init.m
+++ init.m
@@ -3,6 +3,5 @@
 addpath /Users/davidpfau/Documents/MATLAB/tprod % tensor products
 addpath util % utility functions not specific to this project
 addpath nlin % nonlinear functions and their gradients and hessians
-addpath hf-opt % functions related to hessian free optimization and conjugate gradient
 addpath rnn % functions relevant to recurrent NNs and derivatives thereof
 addpath test % scripts that numerically test the various RNN functions
